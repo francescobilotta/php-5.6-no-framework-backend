198d0070e3d33b7463f0301ce5962ec2ca9ef47d
Bilotta Francesco <business@bilottafrancesco.com>
2022-09-23 12:24:01 -0700

    Dispatching to class

--- src/Routes.php
+++ src/Routes.php
@@ -1,18 +1,3 @@
 <?php
 
-return [
-    [
-        'GET',
-        '/hello-world',
-        function () {
-            echo 'Hello World';
-        },
-    ],
-    [
-        'GET',
-        '/another-route',
-        function () {
-            echo 'This works too';
-        },
-    ],
-];
+return [['GET', '/', ['App\Controllers\Homepage', 'show']]];
